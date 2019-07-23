3299ca4fb180040244bf09456de4eaf761af1284
Jaiber Santiago Serna Varela <jaiber.serna@Quipux.local>
2019-07-23 16:20:31 -0500

    tarea: buscar por id.

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -8,10 +8,21 @@ exports.services_all_get = function (req, res) {
         .catch(err => {
             console.log(err)
             res.status(500).send('error')
-            
+
         })
 }
 
 exports.services_detail_get = function (req, res) {
-    res.send('devolver todos los servicios del spa con id' + req.params.id);
+    serviceModel.findAll({
+        attributes: ['id', 'name', 'url', 'happy'],
+        where: {
+            id: req.params.id
+        } 
+    })
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log(err)
+            res.status(500).send('error')
+
+        })
 }
